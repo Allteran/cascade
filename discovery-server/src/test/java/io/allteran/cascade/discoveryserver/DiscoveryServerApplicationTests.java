abcf98a3a70a668092be65a77f24876d0524ef6e
Vitalii Prozapas <vitalii.prozapas@gmail.com>
2022-06-29 15:56:52 +0300

    Minor fixes: rename api-gateway-service to discovery-server

---
+++ discovery-server/src/test/java/io/allteran/cascade/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.allteran.cascade.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
